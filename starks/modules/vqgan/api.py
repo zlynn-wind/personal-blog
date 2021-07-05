e4ad82ccab1aea55943570e0d1c063bfb2786cc8
shonenada <shonenada@gmail.com>
2021-07-05 19:43:06 +0800

    feat: Impl module of vqgan

---
+++ starks/modules/vqgan/api.py
@@ -0,0 +1,4 @@
+from starks.blueprint import Blueprint, render_template
+
+
+bp = Blueprint("vqgan", __name__, url_prefix="/api/v1/vqgan")
---
+++ starks/modules/vqgan/view.py
@@ -0,0 +1,45 @@
+import uuid
+from datetime import datetime
+
+from flask import Blueprint, render_template, request
+
+from starks.modules.vqgan.model.vqgan import VQGANJob
+
+
+bp = Blueprint("vqgan_view", __name__, url_prefix="/vqgan")
+
+
+@bp.route("")
+def index():
+    jobs = VQGANJob.list_latest_jobs()
+    return render_template("vqgan/list.html", jobs=jobs)
+
+
+@bp.route("/<jid>")
+def show_result(jid):
+    job = VQGANJob.query.get(jid)
+    return render_template("vqgan/show.html")
+
+
+@bp.route("", methods=["POST"])
+def create_vqgan():
+    input_text = request.form.get("input_text")
+    today = datetime.now().strftime("%Y%m%d")
+    hex_ = uuid.uuid4().hex
+    filepath = f"/opt/mediakit/public/vqgan/{today}/{hex_}/"
+    volume = f"/opt/mediakit/public/vqgan/{today}/{hex_}/"
+    vqgan = VQGANJob.create(
+        username="surreal",
+        params={
+            "nonce": hex_,
+            "date": today,
+            "input_text": input_text,
+            "docker": {
+                "image": "surreal/vqgan-clip:2021070501",
+                "command": f"'{input_text}'",
+                "volume": volume,
+            },
+        }
+    )
+    jobs = VQGANJob.list_latest_jobs()
+    return render_template("vqgan/list.html", jobs=jobs)
