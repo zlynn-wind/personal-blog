1bb52e718cb8e4a8c4c97f0bb4198a8f519ae54c
shonenada <shonenada@gmail.com>
2021-07-12 11:01:27 +0800

    feat: create k8s job

--- starks/modules/vqgan/dispatcher.py
+++ starks/modules/vqgan/dispatcher.py
@@ -1,6 +1,8 @@
 import time
 import os
 
+from envcfg import asw as aws_cfg
+from envcfg import starks as cfg
 from kubernetes import client, config
 
 from starks.wsgi import application
@@ -8,12 +10,13 @@ from starks.utils.api import external_url_for
 from starks.modules.vqgan.model.vqgan import VQGANJob
 
 
-AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
-AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
-AWS_REGION = os.environ.get("AWS_REGION")
-AWS_EKS_CLUSTER_NAME = os.environ.get("AWS_EKS_CLUSTER_NAME")
-AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
-SERVICE_ENDPOINT = os.environ.get("STARKS_VQGAN_K8S_SERVICE_ENDPOINT")
+AWS_ACCESS_KEY = aws_cfg.ACCESS_KEY
+AWS_SECRET_KEY = aws_cfg.SECRET_KEY
+AWS_REGION = aws_cfg.REGION
+AWS_EKS_CLUSTER_NAME = aws_cfg.EKS_CLUSTER_NAME
+AWS_BUCKET_NAME = aws_cfg.BUCKET_NAME
+SERVICE_ENDPOINT = cfg.VQGAN_K8S_SERVICE_ENDPOINT
+SERVICE_NAMESPACE = cfg.VQGAN_K8S_SERVICE_NAMESPACE
 
 
 def get_oldest_pending_job():
@@ -23,13 +26,41 @@ def get_oldest_pending_job():
                 .order_by(VQGANJob.id).first())
 
 
-def create_k8s_job(job, prehook, posthook):
+def get_k8s_job_name(job):
+    return f"vqgan-{job.id}"
+
+
+def loop_get_job_status(api_instance, job):
+    job_completed = False
+    while not job_completed:
+        api_response = api_instance.read_namespaced_job_status(
+            name=get_k8s_job_name(job),
+            namespace=SERVICE_NAMESPACE)
+        if api_response.status.succeeded is not None or \
+                api_response.status.failed is not None:
+            job_completed = True
+        sleep(1)
+        print("Job status='%s'" % str(api_response.status))
+
+
+def get_job_status(api_instance, job):
+    api_response = api_instance.read_namespaced_job_status(
+        name=get_k8s_job_name(job),
+        namespace=SERVICE_NAMESPACE)
+    if api_response.status.succeeded is not None or \
+            api_response.status.failed is not None:
+        job_completed = True
+    sleep(1)
+    print("Job status='%s'" % str(api_response.status))
+
+
+def make_job_object(api_instance, job, prehook, posthook): 
     params = job.params
     input_text = params.get("input_text")
     docker_args = params.get('docker', None)
     docker_image = docker_args.get("image")
 
-    k8s_job_name = f"vqgan-{job.id}"
+    k8s_job_name = get_k8s_job_name(job)
     container = client.V1Container(
         name=k8s_job_name,
         image=docker_image,
@@ -76,6 +107,14 @@ def create_k8s_job(job, prehook, posthook):
     return k8s_job
 
 
+def create_k8s_job(api_instance, job, prehook, posthook):
+    k8s_job = make_job_object(job, prehook, posthook)
+    api_response = api_instance.create_namespaced_job(
+        body=job, namespace=SERVICE_NAMESPACE)
+    print("Kubernetes job created.")
+    return get_job_status(k8s_job)
+
+
 def validate_job_params(job):
     params = job.params
     input_text = params.get("input_text")
@@ -104,6 +143,7 @@ def validate_job_params(job):
 def main():
     print("Started dispatcher")
     config.load_kube_config()
+    batch_v1 = client.BatchV1Api()
     while True:
         with application.test_request_context():
             PREHOOK_URL = external_url_for(
@@ -121,7 +161,8 @@ def main():
                 continue
             print(f"Found job, id is {job.id}")
             try:
-                create_k8s_job(job, PREHOOK_URL, POSTHOOK_URL)
+                resp = create_k8s_job(batch_v1, job, PREHOOK_URL, POSTHOOK_URL)
+                print(resp)
             except Exception as e:
                 print(f"[ERROR] Failed to dispatch job {job.id}: {e}")
 
